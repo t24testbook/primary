6b809ea96bcb5630cc1a2ab8a3ca90e2994899f9
Hariyanto Tjhin <hariyanto_tjhin@yahoo.com>
2023-01-05 11:08:38 +0800

    Updated example executePlugin

--- executePlugin/example.js
+++ executePlugin/example.js
@@ -1,9 +1,13 @@
 const obj = {
     type: "executePlugin",
     id: "example",
-    arguments: {},
+    description: "This is an example plugin",
+    arguments: [
+        {id: "var1", type: "string", desc: "var1 argument is for ..."},
+        {id: "var2", type: "number", desc: "var2 argument is for ..."}
+    ],
     execute: (base, args) => {
-        console.log(args);
+        console.log(base, args);
     }
 };
 
