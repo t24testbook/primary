ed36f28b8c410e9bbd7ef04fe301a3e5b6ef8787
Hariyanto Tjhin <hariyanto_tjhin@yahoo.com>
2023-01-11 16:33:07 +0800

    extensionQueryPlugin

---
+++ extensionQueryPlugin/example.js
@@ -0,0 +1,17 @@
+const obj = {
+    type: "extensionQueryPlugin",
+    id: "example",
+    description: "This is an example for extension query plugin",
+    author: "Hariyanto Tjhin",
+    version: "1.0",
+    lastUpdate: "3-JAN-2023",
+    arguments: {
+        var1: {type: "list", desc: "var1 argument is for ...", list: ["UP", "DOWN"]},
+        var2: {type: "number", desc: "var2 argument is for ..."}
+    },
+    execute: (base, args) => {
+        console.log(this, base, args);
+    }
+};
+
+window.registerPlugin(obj, window.pluginIndex);
