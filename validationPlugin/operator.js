1ada01c1ca60dd0c21e433e46051b6d298bf1142
Hariyanto Tjhin <hariyanto_tjhin@yahoo.com>
2023-04-29 20:28:10 +0800

    test validationPlugin

---
+++ validationPlugin/operator.js
@@ -0,0 +1,22 @@
+const obj = {
+    type: "validationPlugin",
+    id: "operator",
+    description: "Operator for validating expected and actual results",
+    author: "Hariyanto Tjhin",
+    version: "1.0",
+    lastUpdate: "29-APR-2023",
+    arguments: {
+        operand: {type: "list", desc: "choose which operand", list: ["EQ", "NE", "GT", "GE", "LT", "LE", "CONTAINS", "NOT CONTAINS", "BEGINS WITH", "ENDS WITH"]}
+    },
+    execute: (base, args) => {
+        return this[args.operand](base.actual, base.expected);
+    },
+    EQ: (_act, _exp) => {
+        console.log("EQ", _act, _exp);
+    },
+    NE: (_act, _exp) => {
+        console.log("NE", _act, _exp);
+    }
+};
+
+window.registerPlugin(obj, window.pluginIndex);
