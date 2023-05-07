ac7da4fb27ab1d99f57cabf908c66c01f7c98e89
Hariyanto Tjhin <hariyanto_tjhin@yahoo.com>
2023-05-07 23:34:35 +0800

    fixed comparison

--- validationQueryPlugin/compare.js
+++ validationQueryPlugin/compare.js
@@ -1,9 +1,9 @@
 const obj = {
     type: "validationQueryPlugin",
     id: "compare",
-    description: "This plugin is for comparing actual and expected value with different operator, %...% check if actual is part of the expected value",
+    description: "This plugin is for comparing actual and expected value with different operator, %...% check if actual is part of the expected value. v1.07 fixed numerical compare",
     author: "Hariyanto Tjhin",
-    version: "1.06",
+    version: "1.07",
     lastUpdate: "7-MAY-2023",
     arguments: {
         op: {type: "list", desc: "compare operator", list: ["=", "<>", ">", ">=", "<", "<=", "%...%"]}
@@ -12,11 +12,24 @@ const obj = {
         return {status: "error", message: "test validate error message"};
     },
     execute: (base, args) => {
+        let actual = base.actual;
+        let expected = base.expected;
+
+        if(!isNaN(actual)) {
+            if(actual.indexOf(".") === -1) actual = Math.parseInt(actual)
+            else actual = Math.parseFloat(actual);
+        }
+
+        if(!isNaN(expected)) {
+            if(expected.indexOf(".") === -1) expected = Math.parseInt(expected)
+            else expected = Math.parseFloat(expected);
+        }
+
         switch(args.op) {
             case "=":
-                return {status: "ok", value: (base.actual === base.expected)};
+                return {status: "ok", value: (actual === expected)};
             case "<>":
-                return {status: "ok", value: (base.actual !== base.expected)};
+                return {status: "ok", value: (actual !== expected)};
             case ">":
                 break;
             case ">=":
